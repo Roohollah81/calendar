6c36dcc87ee61d50e80f6685fa843687f3aafb4e
Roohollah81 <m.roohollah.v@gmail.com>
2022-07-12 17:06:58 +0430

    commit

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -28,59 +28,91 @@
     }
   }
 
-  div.cadr {
+  .cadr {
     width: 450px;
-    height: 500px;
+    height: 600px;
     background-color: bisque;
     margin: 2rem auto;
     padding: 15px;
   }
 
-  div.top {
+  .top {
     width: 450px;
     height: 130px;
     background-color: rgb(255, 255, 255);
     position: relative;
   }
-  span.fa {
+  .fa {
     left: 45%;
     top: 23px;
   }
-  span.en {
+  .en {
     left: 35%;
     top: 50px;
   }
-  span.arab {
+  .arab {
     left: 30%;
     top: 75px;
   }
 
-  span.left {
+  .left {
     left: 0;
     padding: 35px !important;
   }
 
-  span.right {
+  .right {
     right: 0;
     padding: 35px !important;
   }
 
-  div.top > span {
+  .top > span {
     position: absolute;
     padding: 15px 5px 0 5px;
   }
 
-  div.navar {
+  .navar {
     height: 35px;
     width: 100%;
-    background-color: gray;
+    background-color: #555;
     border-radius: 5px;
   }
 
-  div.navar > span {
+  .navar > span {
     padding: 0px 22px 0 22px;
+    color: white;
   }
 
+  .day {
+    background-color: rgba(255, 255, 255, 0.8);
+    border: 1px solid rgba(0, 0, 0, 0.8);
+    padding: 20px;
+    font-weight: bold !important;
+    font-size: 22px !important;
+  }
+
+  .month {
+    display: grid;
+    grid-template-columns: auto auto auto auto auto auto auto;
+    background-color: #2196f3;
+    padding: 10px;
+  }
+
+  .jalali {
+    color: gray;
+  }
+
+  .qamari {
+    color: gray;
+    font-size: 8px;
+    float: left;
+  }
+
+  .miladi {
+    color: gray;
+    font-size: 8px;
+    float: right;
+    margin: 1px 0 0;
+  }
 </style>
 
 <main>
@@ -103,5 +135,161 @@
       <span>پ</span>
       <span>ج</span>
     </div>
+    <div class="month">
+      <div class="day">
+        <div class="jalali">۱</div>
+        <div class="miladi">22</div>
+        <div class="qamari">۲۲</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲</div>
+        <div class="miladi">23</div>
+        <div class="qamari">۲۳</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۳</div>
+        <div class="miladi">24</div>
+        <div class="qamari">۲۴</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۴</div>
+        <div class="miladi">25</div>
+        <div class="qamari">۲۵</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۵</div>
+        <div class="miladi">26</div>
+        <div class="qamari">۲۶</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۶</div>
+        <div class="miladi">27</div>
+        <div class="qamari">۲۷</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۷</div>
+        <div class="miladi">28</div>
+        <div class="qamari">۲۸</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۸</div>
+        <div class="miladi">29</div>
+        <div class="qamari">۲۹</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۹</div>
+        <div class="miladi">30</div>
+        <div class="qamari">۳۰</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۰</div>
+        <div class="miladi">1</div>
+        <div class="qamari">۱</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۱</div>
+        <div class="miladi">2</div>
+        <div class="qamari">۲</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۲</div>
+        <div class="miladi">3</div>
+        <div class="qamari">۳</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۳</div>
+        <div class="miladi">4</div>
+        <div class="qamari">۴</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۴</div>
+        <div class="miladi">5</div>
+        <div class="qamari">۵</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۵</div>
+        <div class="miladi">6</div>
+        <div class="qamari">۶</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۶</div>
+        <div class="miladi">7</div>
+        <div class="qamari">۷</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۷</div>
+        <div class="miladi">8</div>
+        <div class="qamari">۸</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۸</div>
+        <div class="miladi">9</div>
+        <div class="qamari">۹</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۱۹</div>
+        <div class="miladi">10</div>
+        <div class="qamari">۱۰</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۰</div>
+        <div class="miladi">11</div>
+        <div class="qamari">۱۱</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۱</div>
+        <div class="miladi">12</div>
+        <div class="qamari">۱۲</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۲</div>
+        <div class="miladi">13</div>
+        <div class="qamari">۱۳</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۳</div>
+        <div class="miladi">14</div>
+        <div class="qamari">۱۴</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۴</div>
+        <div class="miladi">15</div>
+        <div class="qamari">۱۵</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۵</div>
+        <div class="miladi">16</div>
+        <div class="qamari">۱۶</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۶</div>
+        <div class="miladi">27</div>
+        <div class="qamari">۲۷</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۷</div>
+        <div class="miladi">18</div>
+        <div class="qamari">۱۸</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۸</div>
+        <div class="miladi">19</div>
+        <div class="qamari">۱۹</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۲۹</div>
+        <div class="miladi">20</div>
+        <div class="qamari">۲۰</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۳۰</div>
+        <div class="miladi">21</div>
+        <div class="qamari">۲۱</div>
+      </div>
+      <div class="day">
+        <div class="jalali">۳۱</div>
+        <div class="miladi">22</div>
+        <div class="qamari">۲۲</div>
+      </div>
   </div>
 </main>
